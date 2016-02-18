29a91729fca464ec1328409796115cc13ca55708
ChrissW-R1 <contact@ChrissW-R1.me>
2016-02-18 21:45:24 +0100

    + created Maven project

---
+++ src/main/java/me/chrisswr1/parroute/App.java
@@ -0,0 +1,13 @@
+package me.chrisswr1.parroute;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
